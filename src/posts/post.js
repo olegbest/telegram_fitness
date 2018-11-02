42abcdeccb3f2026908bbcb2f62bfd7add08ce54
olegbest <pashkevich.oleg@inbox.ru>
2018-11-02 15:35:10 +0300

    add nodemailer

--- src/posts/post.js
+++ src/posts/post.js
@@ -10,14 +10,14 @@ class Routes {
         this.app.post('/blinger-messages', async function (req, res) {
             res.sendStatus(200);
             if (req.body) {
-                if (req.body['on_message_incoming']) {
-                    let data = req.body['on_message_incoming'];
+                if (req.body.webhook = 'on_message_incoming') {
+                    let data = req.body;
                     let text = `Не прочитанное сообщение от \nid: ${data['from_user'].user_id} \ntitle: ${data['from_user'].title} \napplication: ${data['from_user'].application}`;
-                    let chat_id= -1001348598411;
+                    let chat_id = -1001348598411;
                     await messageUtils.sendText(bot, chat_id, text);
                 }
+                console.log(req.body);
             }
-            console.log(req.body);
         })
     }
 }
