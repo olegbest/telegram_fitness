0bd3c942478755dac13c75fb0899ede18c8c35bd
olegbest <pashkevich.oleg@inbox.ru>
2018-11-02 15:46:06 +0300

    add nodemailer

--- src/posts/post.js
+++ src/posts/post.js
@@ -11,9 +11,9 @@ class Routes {
             res.sendStatus(200);
             if (req.body) {
                 console.log(req.body);
-                if (req.body.webhook = 'on_message_incoming') {
-                    let data = req.body;
-                    let text = `Не прочитанное сообщение от \nid: ${data['from_user'].user_id} \ntitle: ${data['from_user'].title} \napplication: ${data['from_user'].application}`;
+                if (req.body.webhook === 'on_conversation_unassigned') {
+                    let data = req.body.conversation;
+                    let text = `Не прочитанное сообщение от \nid: ${data['from_user'].user_id} \ntitle: ${data['from_user'].title} \napplication: ${data.application}`;
                     let chat_id = -1001348598411;
                     await messageUtils.sendText(bot, chat_id, text);
                 }
